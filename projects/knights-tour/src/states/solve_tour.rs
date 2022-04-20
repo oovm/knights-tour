38cae155c07320c168cd5ab7c28ca313be30895d
Aster <aster@vers.site>
2022-04-20 15:58:18 +0000

    🚀 Optimize available moves

--- projects/knights-tour/src/states/solve_tour.rs
+++ projects/knights-tour/src/states/solve_tour.rs
@@ -1,6 +1,6 @@
 use super::*;
 use itertools::Itertools;
-use rand::{prelude::SliceRandom, thread_rng};
+use rand::{prelude::SliceRandom, rngs::SmallRng, SeedableRng};
 
 impl ChessTourState {
     pub fn get_visited(&self, x: isize, y: isize) -> bool {
@@ -34,27 +34,25 @@ impl ChessTourState {
 }
 
 impl ChessTourState {
-    pub fn moves_available(&self) -> Vec<(isize, isize)> {
-        self.available_moves
-            .iter()
-            .filter_map(|(dx, dy)| {
-                let x = self.current_x + dx;
-                let y = self.current_y + dy;
-                if x < 0 || y < 0 || x >= self.size_x || y >= self.size_y {
-                    return None;
+    pub fn available_moves(&self) -> Vec<(isize, isize)> {
+        let mut moves = vec![];
+        for (x, y) in &self.available_moves {
+            let x = self.current_x + x;
+            let y = self.current_y + y;
+            if x < 0 || y < 0 || x >= self.size_x || y >= self.size_y {
+                continue;
+            }
+            if self.must_back_to_start() {
+                if (x, y) != self.path[0] {
+                    continue;
                 }
-                if self.must_back_to_start() {
-                    // println!("must_back_to_start: {} {} {} {}", x, y, self.path[0].0, self.path[0].1);
-                    if (x, y) != self.path[0] {
-                        return None;
-                    }
-                }
-                else if self.get_visited(x, y) {
-                    return None;
-                }
-                Some((x, y))
-            })
-            .collect()
+            }
+            else if self.get_visited(x, y) {
+                continue;
+            }
+            moves.push((x, y));
+        }
+        moves
     }
     pub fn backtrack(self) -> impl Iterator<Item = Self> {
         let mut stack = vec![self];
@@ -68,7 +66,7 @@ impl ChessTourState {
                     yield state;
                     continue;
                 }
-                for (x, y) in state.moves_available() {
+                for (x, y) in state.available_moves() {
                     state.go_grid(x, y);
                     stack.push(state.clone());
                     state.go_back();
@@ -80,9 +78,9 @@ impl ChessTourState {
 
 impl ChessTourState {
     pub fn best_moves(&self) -> Vec<(isize, isize)> {
-        let mut moves_scores = vec![];
+        let mut moves = vec![];
         let mut min_score = usize::MAX;
-        for (x, y) in self.moves_available() {
+        for (x, y) in self.available_moves() {
             let mut score = 0;
             for (dx, dy) in self.available_moves.iter() {
                 let nx = x + dx;
@@ -94,14 +92,14 @@ impl ChessTourState {
             if score < min_score {
                 min_score = score;
             }
-            moves_scores.push((x, y, score));
+            moves.push((x, y, score));
         }
-        moves_scores.into_iter().filter(|(_, _, score)| *score == min_score).map(|(x, y, _)| (x, y)).collect_vec()
+        moves.into_iter().filter(|(_, _, score)| *score == min_score).map(|(x, y, _)| (x, y)).collect_vec()
     }
 
     pub fn warnsdorff_rule(&self) -> impl Iterator<Item = Self> {
         let state = self.clone();
-        let mut rng = thread_rng();
+        let mut rng = SmallRng::from_entropy();
         from_generator(move || {
             'outer: loop {
                 let mut state = state.clone();
@@ -109,10 +107,11 @@ impl ChessTourState {
                     let best_moves = state.best_moves();
                     match best_moves.choose(&mut rng) {
                         Some((x, y)) => state.go_grid(*x, *y),
+                        // 最佳走法为空，说明已经走到死胡同，回溯也没用, 直接推倒重来
                         None => continue 'outer,
                     }
                 }
-                yield state
+                yield state;
             }
         })
     }

9179554104ec1bbe9a64574e47a393b5467b5092
Aster <aster@vers.site>
2023-03-26 10:35:10 +0800

    Update readme

--- projects/hamiltonian/src/lib.rs
+++ projects/hamiltonian/src/lib.rs
@@ -1,9 +1,7 @@
 #![feature(generators)]
+#![feature(iter_from_generator)]
 
-use std::io::Read;
-
-// mod states;
-use gen_iter::GenIter;
+use std::iter::from_generator;
 
 /// A vertex in graph
 pub trait Hamiltonian
@@ -12,35 +10,26 @@ where
 {
     fn count(&self) -> usize;
     fn current(&self) -> usize;
-    /// get visited
-    fn get_visited(&self, node: usize) -> bool;
-    /// set visited
-    fn set_visited(&mut self, node: usize, state: bool);
     /// walk to node
-    fn walk_to(&mut self, node: usize);
+    fn go_walk(&mut self, node: usize);
     /// back to node
-    fn back_to(&mut self, node: usize);
-    /// all valid moves
+    fn go_back(&mut self, steps: usize);
+    /// all valid moves, should also check if the node is visited
     fn possible_moves(&self, start: usize) -> Vec<usize>;
 }
 
-pub fn backtracking<T: Hamiltonian>(mut graph: T) -> impl Iterator<Item = Vec<usize>> {
+pub fn backtracking<T: Hamiltonian>(graph: T) -> impl Iterator<Item = Vec<usize>> {
     let mut stack: Vec<(Vec<usize>, usize)> = vec![(vec![graph.current()], graph.current())];
-    // let mut paths: Vec<Vec<usize>> = vec![];
-    GenIter(move || {
+    from_generator(move || {
         while let Some((path, current)) = stack.pop() {
             if path.len() == graph.count() {
                 yield path;
             }
             else {
                 for next in graph.possible_moves(current) {
-                    if !graph.get_visited(next) {
-                        let mut new_path = path.clone();
-                        new_path.push(next);
-                        let mut new_self = graph.clone();
-                        new_self.set_visited(next, true);
-                        stack.push((new_path, next));
-                    }
+                    let mut path = path.clone();
+                    path.push(next);
+                    stack.push((path, next));
                 }
             }
         }
